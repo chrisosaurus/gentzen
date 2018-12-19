1724d1772ffd1686bc320979212c3e4675632094
Chris Hall <followingthepath@gmail.com>
2018-12-19 19:41:29 +1100

    Adding new Proof.Run2

---
+++ src/Proof/Run2.hs
@@ -0,0 +1,89 @@
+module Proof.Run2
+(
+    run,
+)
+where
+
+
+import Sequent.Data.Sequent (Sequent)
+import System.Apply (apply)
+import Proof.Data.Theorem
+import qualified Proof.Data.Proof as Proof
+import System.Data.System
+
+check_system :: System -> Theorem -> Either String ()
+check_system sy th = do
+    sy_name    <- return $ system_name sy
+    th_sy_name <- return $ system th
+    if sy_name == th_sy_name
+        then Right ()
+        else Left $
+            "Wrong system supplied: wanted '" ++ th_sy_name ++ "', but got '" ++ sy_name ++ "'"
+
+run :: System -> Theorem -> Either String ([String], Proof.Proof)
+run sy th = do
+    ()            <- check_system sy th
+    steps         <- return $ steps th
+    seq           <- return $ sequent th
+    proof         <- return $ Proof.start seq
+    (strs, proof) <- run' sy proof steps
+    if Proof.finished proof
+        then Right ([], proof)
+        else Left $
+            "Failed to discharge remaining proof obligations: " ++
+            (show (Proof.unproven proof)) ++ "\n" ++ (show proof)
+
+run' :: System -> Proof.Proof -> [Stmt] -> Either String ([String], Proof.Proof)
+run' _  proof [] = Right ([], proof)
+run' sy proof (stmt:stmts) = case stmt of
+    Branch lstmts rstmts -> do
+        -- branch should mean we have no remaining stmts
+        -- TODO FIXME make the error message here prettier
+        []             <- return stmts
+        -- internally it is a *stack* so we must do right branch first
+        (rstrs, proof) <- run' sy proof rstmts
+        (lstrs, proof) <- run' sy proof lstmts
+        return (lstrs ++ rstrs, proof)
+
+    Apply name args -> do
+        id     <- Proof.tip proof
+        iseq   <- Proof.get id proof
+        rule   <- lookup_rule sy name
+        oseqs  <- apply iseq rule args
+        (ids, proof)  <- return $ Proof.addSeqs oseqs proof
+        step   <- case ids of
+            []     -> Right $ Proof.Axiom    id name args
+            x:[]   -> Right $ Proof.Straight id name args x
+            x:y:[] -> Right $ Proof.Split    id name args x y
+            _      -> Left $ "Error: too many ids, found '" ++ (show (length ids)) ++ "'"
+        proof         <- return $ Proof.addStep step proof
+        run' sy proof stmts
+
+    Expect sexp -> do
+        id  <- Proof.tip proof
+        seq <- Proof.get id proof
+        ()  <- expect sexp seq
+        run' sy proof stmts
+
+    Print -> do
+        id            <- Proof.tip proof
+        seq           <- Proof.get id proof
+        str           <- return $ show seq
+        (strs, proof) <- run' sy proof stmts
+        return (str:strs, proof)
+
+    Abort -> do
+        -- abort should mean we have no remaining stmts
+        -- TODO FIXME make the error message here prettier
+        []    <- return stmts
+        id    <- Proof.tip proof
+        proof <- return $ Proof.abortSeq id proof
+        step  <- return $ Proof.Abort id
+        proof <- return $ Proof.addStep step proof
+        return ([], proof)
+
+expect :: Sequent -> Sequent -> Either String ()
+expect exp got | exp == got = Right ()
+expect exp got              = Left $
+    "Expectaton failed\n\texpected: " ++ (show exp) ++ "\n\tgot: " ++ (show got)
+
