96c49b132de2d34cf039a4fc63408d80204eb043
Chris Hall <followingthepath@gmail.com>
2018-12-19 19:41:29 +1100

    Implementing System.Check

--- src/System/Check.hs
+++ src/System/Check.hs
@@ -8,15 +8,45 @@ import Sequent.Data.Sequent
 import System.Data.Rewrite
 import System.Data.Rule
 
+-- checks to do:
+--  every symbol in args is unique
+--  left_name and right_name are unique and not in args
+--  every symbol mentioned in props is mentioned in args, left_name, or right_name
+--  every symbol mentioned in body is mentioned in args, left_name, or right_name
+
 -- check that a given rule is well formed
 check :: Rule -> Either String ()
-check Rule{ rule_name = ruleName
+check Rule{ rule_name = rule_name
           , args = args
           , left_name = left_name
           , right_name = right_name
           , props = props
           , body = body
-          } = Left "System.Check.check unimplemented"
+          } = do
+    arg_symbols <- return $ extract_sequent_list_symbols args
+    env_symbols <- return $ (left_name:right_name:arg_symbols)
+    () <- all_unique rule_name env_symbols
+    props_symbols <- return $ extract_props_symbols props
+    () <- check_defined rule_name props_symbols env_symbols
+    body_symbols <- return $ extract_body_symbols body
+    () <- check_defined rule_name body_symbols env_symbols
+    return ()
+
+-- check that every string is unique (appears only once)
+all_unique :: String -> [String] -> Either String ()
+all_unique _         []                   = Right ()
+all_unique rule_name (x:xs) | x `elem` xs = Left $ "in rule '" ++ rule_name ++ "': argument '" ++ x ++ "' is repeated."
+all_unique rule_name (x:xs)               = all_unique rule_name xs
+
+-- check that every symbol in left list is defined in right list
+check_defined :: String -> [String] -> [String] -> Either String ()
+check_defined _                            [] _ = Right ()
+check_defined rule_name (x:xs) ys | x `elem` ys = check_defined rule_name xs ys
+check_defined rule_name (x:xs) ys               = Left $ "in rule '" ++ rule_name ++ "': symbol '" ++ x ++ "' used in rewrite rule but not defined."
+
+
+extract_sequent_list_symbols :: [Exp] -> [String]
+extract_sequent_list_symbols exps = concat $ map extract_sequent_symbols exps
 
 extract_sequent_symbols :: Exp -> [String]
 extract_sequent_symbols Bottom        = []
@@ -56,9 +86,4 @@ extract_expr_symbols (Remove sexp) = extract_sequent_symbols sexp
 
 
 
--- checks to do:
---  every symbol in args is unique
---  left_name and right_name are unique and not in args
---  every symbol mentioned in props is mentioned in args, left_name, or right_name
---  every symbol mentioned in body is mentioned in args, left_name, or right_name
 
---
+++ test/System/CheckSpec.hs
@@ -0,0 +1,264 @@
+module System.CheckSpec
+(
+    main,
+    spec,
+)
+where
+
+import Test.Hspec
+
+import Sequent.Data.Sequent
+import System.Check
+import System.Data.Rewrite
+
+spec :: Spec
+spec = do
+  describe "success cases" $ do
+    it "empty success" $ do
+      let system = Rule { rule_name = "boring rule"
+                        , args = []
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = []
+                        , body = Unit
+                        }
+      let expected = Right $ ()
+      check system `shouldBe` expected
+
+    it "symbol in prop" $ do
+      let system = Rule { rule_name = "defined prop symbol"
+                        , args = [Symbol "a"]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = [In "a" "L"]
+                        , body = Unit
+                        }
+      let expected = Right ()
+      check system `shouldBe` expected
+
+    it "axiom" $ do
+      let system = Rule { rule_name = "axiom"
+                        , args = [Symbol "a"]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = [In "a" "L", In "a" "R"]
+                        , body = Unit
+                        }
+      let expected = Right ()
+      check system `shouldBe` expected
+
+    it "left_and" $ do
+      let lterm = Expr "L" [ Remove (And (Symbol "a") (Symbol "b"))
+                           , Add (Symbol "a")
+                           , Add (Symbol "b")
+                           ]
+      let rterm = SExp (Symbol "R")
+      let body = Single (Sequ lterm rterm)
+      let system = Rule { rule_name = "left_and"
+                        , args = [And (Symbol "a") (Symbol "b")]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = []
+                        , body = body
+                        }
+      let expected = Right ()
+      check system `shouldBe` expected
+
+    it "right_and" $ do
+      let llterm = Expr "L" [Add (Symbol "a")]
+      let rlterm = Expr "L" [Add (Symbol "b")]
+      let rterm = Expr "R" [Remove (And (Symbol "a") (Symbol "b"))]
+      let lsequ = Sequ llterm rterm
+      let rsequ = Sequ rlterm rterm
+      let body = Pair lsequ rsequ
+      let system = Rule { rule_name = "right_and"
+                        , args = [And (Symbol "a") (Symbol "b")]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = []
+                        , body = body
+                        }
+      let expected = Right ()
+      check system `shouldBe` expected
+
+
+
+
+  describe "error cases" $ do
+    it "repeated argument error 1" $ do
+      let system = Rule { rule_name = "repeating argument"
+                        , args = [Symbol "a", Symbol "b", Symbol "a"]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = []
+                        , body = Unit
+                        }
+      let expected = Left $ "in rule 'repeating argument': argument 'a' is repeated."
+      check system `shouldBe` expected
+
+    it "repeated argument error 2" $ do
+      let system = Rule { rule_name = "repeating argument"
+                        , args = [And (Symbol "a") (Symbol "a")]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = []
+                        , body = Unit
+                        }
+      let expected = Left $ "in rule 'repeating argument': argument 'a' is repeated."
+      check system `shouldBe` expected
+
+    it "repeated argument error 3" $ do
+      let system = Rule { rule_name = "repeating argument"
+                        , args = [Implies (Symbol "a") (Symbol "b"), Implies (Symbol "b") (Symbol "c")]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = []
+                        , body = Unit
+                        }
+      let expected = Left $ "in rule 'repeating argument': argument 'b' is repeated."
+      check system `shouldBe` expected
+
+    it "repeated sequent name error" $ do
+      let system = Rule { rule_name = "repeating sequent name"
+                        , args = []
+                        , left_name = "L"
+                        , right_name = "L"
+                        , props = []
+                        , body = Unit
+                        }
+      let expected = Left $ "in rule 'repeating sequent name': argument 'L' is repeated."
+      check system `shouldBe` expected
+
+    it "non unique sequent name error" $ do
+      let system = Rule { rule_name = "non-unique sequent name"
+                        , args = [Symbol "a"]
+                        , left_name = "L"
+                        , right_name = "a"
+                        , props = []
+                        , body = Unit
+                        }
+      let expected = Left $ "in rule 'non-unique sequent name': argument 'a' is repeated."
+      check system `shouldBe` expected
+
+    it "left undefined symbol in prop" $ do
+      let system = Rule { rule_name = "left undefined prop symbol"
+                        , args = [Symbol "a"]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = [In "b" "L"]
+                        , body = Unit
+                        }
+      let expected = Left $ "in rule 'left undefined prop symbol': symbol 'b' used in rewrite rule but not defined."
+      check system `shouldBe` expected
+
+    it "right undefined symbol in prop" $ do
+      let system = Rule { rule_name = "right undefined prop symbol"
+                        , args = [Symbol "a"]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = [In "a" "M"]
+                        , body = Unit
+                        }
+      let expected = Left $ "in rule 'right undefined prop symbol': symbol 'M' used in rewrite rule but not defined."
+      check system `shouldBe` expected
+
+    it "undefined symbol in body 1" $ do
+      let lterm = SExp (Symbol "e")
+      let rterm = Empty
+      let body = Single (Sequ lterm rterm)
+      let system = Rule { rule_name = "test"
+                        , args = [Symbol "a"]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = []
+                        , body = body
+                        }
+      let expected = Left $ "in rule 'test': symbol 'e' used in rewrite rule but not defined."
+      check system `shouldBe` expected
+
+    it "undefined symbol in body 2" $ do
+      let lterm = Empty
+      let rterm = SExp (Symbol "e")
+      let body = Single (Sequ lterm rterm)
+      let system = Rule { rule_name = "test"
+                        , args = [Symbol "a"]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = []
+                        , body = body
+                        }
+      let expected = Left $ "in rule 'test': symbol 'e' used in rewrite rule but not defined."
+      check system `shouldBe` expected
+
+    it "undefined symbol in body 3" $ do
+      let lterm = Empty
+      let rterm = Expr "e" []
+      let body = Single (Sequ lterm rterm)
+      let system = Rule { rule_name = "test"
+                        , args = [Symbol "a"]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = []
+                        , body = body
+                        }
+      let expected = Left $ "in rule 'test': symbol 'e' used in rewrite rule but not defined."
+      check system `shouldBe` expected
+
+    it "undefined symbol in body 4" $ do
+      let lterm = Empty
+      let rterm = Expr "L" [ Add (Symbol "a")
+                           , Add (Symbol "b")
+                           , Add (Symbol "L")
+                           , Add (Symbol "e")
+                           , Add (Symbol "R")
+                           ]
+      let body = Single (Sequ lterm rterm)
+      let system = Rule { rule_name = "test"
+                        , args = [And (Symbol "a") (Symbol "b")]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = []
+                        , body = body
+                        }
+      let expected = Left $ "in rule 'test': symbol 'e' used in rewrite rule but not defined."
+      check system `shouldBe` expected
+
+    it "faulty left_and" $ do
+      let lterm = Expr "L" [ Remove (And (Symbol "a") (Symbol "b"))
+                           , Add (Symbol "a")
+                           , Add (Symbol "b")
+                           , Add (Symbol "c")
+                           ]
+      let rterm = SExp (Symbol "R")
+      let body = Single (Sequ lterm rterm)
+      let system = Rule { rule_name = "faulty left_and"
+                        , args = [And (Symbol "a") (Symbol "b")]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = []
+                        , body = body
+                        }
+      let expected = Left $ "in rule 'faulty left_and': symbol 'c' used in rewrite rule but not defined."
+      check system `shouldBe` expected
+
+    it "faulty right_and" $ do
+      let llterm = Expr "L" [Add (Symbol "a")]
+      let rlterm = Expr "L" [Add (Symbol "c")]
+      let rterm = Expr "R" [Remove (And (Symbol "a") (Symbol "b"))]
+      let lsequ = Sequ llterm rterm
+      let rsequ = Sequ rlterm rterm
+      let body = Pair lsequ rsequ
+      let system = Rule { rule_name = "faulty right_and"
+                        , args = [And (Symbol "a") (Symbol "b")]
+                        , left_name = "L"
+                        , right_name = "R"
+                        , props = []
+                        , body = body
+                        }
+      let expected = Left $ "in rule 'faulty right_and': symbol 'c' used in rewrite rule but not defined."
+      check system `shouldBe` expected
+
+
+main :: IO ()
+main = hspec spec
+
