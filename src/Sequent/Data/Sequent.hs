c378e300d1d462cebe84cc971c4c88bce09e5e95
Chris Hall <followingthepath@gmail.com>
2018-12-19 19:41:29 +1100

    Adding custom show for Sequent.Exp and Sequent.Sequent

--- src/Sequent/Data/Sequent.hs
+++ src/Sequent/Data/Sequent.hs
@@ -10,7 +10,17 @@ data Exp = And       Exp Exp
          | Implies   Exp Exp
          | Bracketed Exp
          | Symbol    String
-    deriving (Show, Eq)
+    deriving (Eq)
 
 data Sequent = Sequent [Exp] [Exp]
-    deriving (Show, Eq)
+    deriving (Eq)
+
+instance Show Exp where
+    show (And l r)     = (show l) ++ "^"  ++ (show r)
+    show (Or l r)      = (show l) ++ "v"  ++ (show r)
+    show (Implies l r) = (show l) ++ "->" ++ (show r)
+    show (Bracketed e) = "(" ++ (show e) ++ ")"
+    show (Symbol s)    = s
+
+instance Show Sequent where
+    show (Sequent lhs rhs) = (show lhs) ++ " |- " ++ (show rhs)
